0eaebcf14468ff5c0e00997e4dc100f9513d50fa
Or Tslil <ortslil64@gmail.com>
2019-08-06 16:33:05 +0300

    u

---
+++ openai-ros-gazebo-catch-env/openai_ros_multirobot/src/openai_ros/robot_envs/multirobot_catch_env.py
@@ -0,0 +1,300 @@
+import numpy
+import rospy
+import time
+from openai_ros import robot_gazebo_env
+from std_msgs.msg import Float64
+from sensor_msgs.msg import JointState
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import PointCloud2
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from openai_ros.openai_ros_common import ROSLauncher
+from gazebo_msgs.msg import ModelStates
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from sensor_msgs.msg import Image
+
+
+class TurtleBot2catchEnv(robot_gazebo_env.RobotGazeboEnv):
+    """Superclass for all CubeSingleDisk environments.
+    """
+
+    def __init__(self, ros_ws_abspath):
+        
+        self.bridge = CvBridge()
+        rospy.logdebug("Start TurtleBot2catchEnv INIT...")
+        # Variables that we give through the constructor.
+        # None in this case
+
+        # We launch the ROSlaunch that spawns the robot into the world
+        ROSLauncher(rospackage_name="dql_robot",
+                    launch_file_name="put_robots_in_world.launch",
+                    ros_ws_abspath=ros_ws_abspath)
+
+        ROSLauncher(rospackage_name="dql_robot",
+                    launch_file_name="put_prey_in_world.launch",
+                    ros_ws_abspath=ros_ws_abspath)
+
+        # Internal Vars
+        # Doesnt have any accesibles
+        self.controllers_list = []
+
+        # It doesnt use namespace
+        self.robot_name_space = ""
+
+        # We launch the init function of the Parent Class robot_gazebo_env.RobotGazeboEnv
+        super(TurtleBot2catchEnv, self).__init__(controllers_list=self.controllers_list,
+                                            robot_name_space=self.robot_name_space,
+                                            reset_controls=False,
+                                            start_init_physics_parameters=False,
+                                            reset_world_or_sim="WORLD")
+
+
+
+
+        self.gazebo.unpauseSim()
+        #self.controllers_object.reset_controllers()
+        self._check_all_sensors_ready()
+
+        # We Start all the ROS related Subscribers and publishers
+        rospy.Subscriber("/robot1/odom", Odometry, self._odom_callback1)
+        rospy.Subscriber("/robot2/odom", Odometry, self._odom_callback2)
+        rospy.Subscriber("/robot3/odom", Odometry, self._odom_callback3)
+        rospy.Subscriber("/robot1/camera/rgb/image_raw", Image, self._camera_rgb_image_raw_callback1)
+        rospy.Subscriber("/robot2/camera/rgb/image_raw", Image, self._camera_rgb_image_raw_callback2)
+        rospy.Subscriber("/robot3/camera/rgb/image_raw", Image, self._camera_rgb_image_raw_callback3)
+        
+
+        self._cmd_vel_pub1 = rospy.Publisher('/robot1/cmd_vel', Twist, queue_size=1)
+        self._cmd_vel_pub2 = rospy.Publisher('/robot2/cmd_vel', Twist, queue_size=1)
+        self._cmd_vel_pub3 = rospy.Publisher('/robot3/cmd_vel', Twist, queue_size=1)
+        self._cmd_vel_pub_prey = rospy.Publisher('/prey/cmd_vel', Twist, queue_size=1)
+
+        rospy.Subscriber("/gazebo/model_states", ModelStates ,self._model_state_callback)
+        #rospy.Subscriber("/camera/depth/image_raw", Image, self._camera_depth_image_raw_callback)
+        #rospy.Subscriber("/camera/depth/points", PointCloud2, self._camera_depth_points_callback)
+        
+        
+
+        
+
+        self._check_publishers_connection()
+
+        self.gazebo.pauseSim()
+        
+        rospy.logdebug("Finished TurtleBot2Env INIT...")
+        
+
+    # Methods needed by the RobotGazeboEnv
+    # ----------------------------
+    
+    def _model_state_callback(self,msg):
+        models = msg.name
+        robot_idx1 = models.index('mobile_base1')
+        robot_idx2 = models.index('mobile_base2')
+        robot_idx3 = models.index('mobile_base3')
+        prey_idx = models.index('prey')
+        self.robot_position1 = [msg.pose[robot_idx1].position.x, msg.pose[robot_idx1].position.y]
+        self.robot_position2 = [msg.pose[robot_idx2].position.x, msg.pose[robot_idx2].position.y]
+        self.robot_position3 = [msg.pose[robot_idx3].position.x, msg.pose[robot_idx3].position.y]
+        self.prey_position = [msg.pose[prey_idx].position.x, msg.pose[prey_idx].position.y]
+
+    def _check_all_systems_ready(self):
+        """
+        Checks that all the sensors, publishers and other simulation systems are
+        operational.
+        """
+        self._check_all_sensors_ready()
+        return True
+
+
+    # CubeSingleDiskEnv virtual methods
+    # ----------------------------
+
+    def _check_all_sensors_ready(self):
+        rospy.logdebug("START ALL SENSORS READY")
+        self._check_odom_ready()
+        rospy.logdebug("ALL SENSORS READY")
+
+    def _check_odom_ready(self):
+        self.odom1 = None
+        self.odom2 = None
+        self.odom3 = None
+        rospy.logdebug("Waiting for /odom to be READY...")
+        while (self.odom1 is None or self.odom2 is None or self.odom3 is None) and not rospy.is_shutdown():
+            try:
+                self.odom1 = rospy.wait_for_message("/robot1/odom", Odometry, timeout=5.0)
+                self.odom2 = rospy.wait_for_message("/robot2/odom", Odometry, timeout=5.0)
+                self.odom3 = rospy.wait_for_message("/robot3/odom", Odometry, timeout=5.0)
+                rospy.logdebug("Current /odom READY=>")
+
+            except:
+                rospy.logerr("Current /odom not ready yet, retrying for getting odom")
+
+        return self.odom1
+        
+        
+
+        
+
+    def _odom_callback1(self, data):
+        self.odom1 = data
+
+    def _odom_callback2(self, data):
+        self.odom2 = data
+
+    def _odom_callback3(self, data):
+        self.odom3 = data
+        
+    def _camera_rgb_image_raw_callback1(self, data):
+        self.camera_rgb_image_raw1 = self.bridge.imgmsg_to_cv2(data,"rgb8")
+
+    def _camera_rgb_image_raw_callback2(self, data):
+        self.camera_rgb_image_raw2 = self.bridge.imgmsg_to_cv2(data,"rgb8")
+
+    def _camera_rgb_image_raw_callback3(self, data):
+        self.camera_rgb_image_raw3 = self.bridge.imgmsg_to_cv2(data,"rgb8")
+            
+    def _laser_scan_callback1(self, data):
+        self.laser_scan1 = data
+
+    def _laser_scan_callback2(self, data):
+        self.laser_scan2 = data
+
+    def _laser_scan_callback3(self, data):
+        self.laser_scan3 = data
+
+        
+    def _check_publishers_connection(self):
+        """
+        Checks that all the publishers are working
+        :return:
+        """
+        rate = rospy.Rate(10)  # 10hz
+        while (self._cmd_vel_pub1.get_num_connections() == 0 or self._cmd_vel_pub2.get_num_connections() == 0 or self._cmd_vel_pub3.get_num_connections() == 0 ) and not rospy.is_shutdown():
+            rospy.logdebug("No susbribers to _cmd_vel_pub yet so we wait and try again")
+            try:
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                # This is to avoid error when world is rested, time when backwards.
+                pass
+        rospy.logdebug("_cmd_vel_pub Publisher Connected")
+
+        rospy.logdebug("All Publishers READY")
+    
+    # Methods that the TrainingEnvironment will need to define here as virtual
+    # because they will be used in RobotGazeboEnv GrandParentClass and defined in the
+    # TrainingEnvironment.
+    # ----------------------------
+    def _set_init_pose(self):
+        """Sets the Robot in its init pose
+        """
+        raise NotImplementedError()
+    
+    def _init_env_variables(self):
+        """Inits variables needed to be initialised each time we reset at the start
+        of an episode.
+        """
+        raise NotImplementedError()
+
+    def _compute_reward(self, observations, done):
+        """Calculates the reward to give based on the observations given.
+        """
+        raise NotImplementedError()
+
+    def _set_action(self, action):
+        """Applies the given action to the simulation.
+        """
+        raise NotImplementedError()
+
+    def _get_obs(self):
+        raise NotImplementedError()
+
+    def _is_done(self, observations):
+        """Checks if episode done based on observations given.
+        """
+        raise NotImplementedError()
+        
+    # Methods that the TrainingEnvironment will need.
+    # ----------------------------
+    def move_base(self,robot_id, linear_speed, angular_speed,sleep_time = 0.2, epsilon=0.05, update_rate=10, min_laser_distance=-1):
+        """
+        It will move the base based on the linear and angular speeds given.
+        It will wait untill those twists are achived reading from the odometry topic.
+        :param linear_speed: Speed in the X axis of the robot base frame
+        :param angular_speed: Speed of the angular turning of the robot base frame
+        :param epsilon: Acceptable difference between the speed asked and the odometry readings
+        :param update_rate: Rate at which we check the odometry.
+        :return: 
+        """
+        cmd_vel_value = Twist()
+        cmd_vel_value.linear.x = linear_speed
+        cmd_vel_value.angular.z = angular_speed
+        rospy.logdebug("TurtleBot2 Base Twist Cmd>>" + str(cmd_vel_value))
+        self._check_publishers_connection()
+        if robot_id == 1:
+            self._cmd_vel_pub1.publish(cmd_vel_value)
+        if robot_id == 2:
+            self._cmd_vel_pub2.publish(cmd_vel_value)
+        if robot_id == 3:
+            self._cmd_vel_pub3.publish(cmd_vel_value)
+        time.sleep(sleep_time)
+
+
+    def _move_prey(self, linear_speed, angular_speed,sleep_time = 0.2):
+        cmd_vel_value = Twist()
+        cmd_vel_value.linear.x = linear_speed
+        cmd_vel_value.angular.z = angular_speed
+        self._cmd_vel_pub_prey.publish(cmd_vel_value)
+        time.sleep(sleep_time)
+
+    def _prey_step(self):
+        linear_speed = numpy.random.uniform(-0.2,0.5)
+        angular_speed = numpy.random.uniform(-0.5,0.5)
+        self._move_prey(linear_speed, angular_speed, sleep_time=0.5)
+                        
+        
+
+    def get_odom(self, robot_id):
+        if robot_id == 1:
+            return self.odom1
+        if robot_id == 2:
+            return self.odom2
+        if robot_id == 3:
+            return self.odom3
+        
+    def get_camera_rgb_image_raw(self, robot_id):
+        if robot_id == 1:
+            return self.camera_rgb_image_raw1
+        if robot_id == 2:
+            return self.camera_rgb_image_raw2
+        if robot_id == 3:
+            return self.camera_rgb_image_raw3
+        
+    def get_laser_scan(self, robot_id):
+        if robot_id == 1:
+            return self.laser_scan1
+        if robot_id == 2:
+            return self.laser_scan2
+        if robot_id == 3:
+            return self.laser_scan3
+
+    def get_prey_position(self):
+        return self.prey_position
+
+    def get_robot_position(self, robot_id):
+        if robot_id == 1:
+            return self.robot_position1
+        if robot_id == 2:
+            return self.robot_position2
+        if robot_id == 3:
+            return self.robot_position3
+        
+    def reinit_sensors(self):
+        """
+        This method is for the tasks so that when reseting the episode
+        the sensors values are forced to be updated with the real data and 
+        
+        """
+        
---
+++ openai-ros-gazebo-catch-env/openai_ros_multirobot/src/openai_ros/task_envs/multirobot_catch/multirobot_catchEnv.py
@@ -0,0 +1,198 @@
+import rospy
+import numpy
+from gym import spaces
+from openai_ros.robot_envs import multirobot_catch_env
+from gym.envs.registration import register
+from geometry_msgs.msg import Point
+from openai_ros.task_envs.task_commons import LoadYamlFileParamsTest
+from openai_ros.openai_ros_common import ROSLauncher
+import os
+
+
+class CatchEnv(multirobot_catch_env.TurtleBot2catchEnv):
+    def __init__(self):
+        """
+        This Task Env is designed for having the TurtleBot2 in some kind of maze.
+        It will learn how to move around the maze without crashing.
+        """
+
+        # This is the path where the simulation files, the Task and the Robot gits will be downloaded if not there
+        ros_ws_abspath = rospy.get_param("/turtlebot2/ros_ws_abspath", None)
+        assert ros_ws_abspath is not None, "You forgot to set ros_ws_abspath in your yaml file of your main RL script. Set ros_ws_abspath: \'YOUR/SIM_WS/PATH\'"
+        assert os.path.exists(ros_ws_abspath), "The Simulation ROS Workspace path " + ros_ws_abspath + \
+                                               " DOESNT exist, execute: mkdir -p " + ros_ws_abspath + \
+                                               "/src;cd " + ros_ws_abspath + ";catkin_make"
+
+        ROSLauncher(rospackage_name="dql_robot",
+                    launch_file_name="catch_world.launch",
+                    ros_ws_abspath=ros_ws_abspath)
+
+        rospy.logdebug("finish loading sumo_world.launch")
+
+        # Load Params from the desired Yaml file
+        LoadYamlFileParamsTest(rospackage_name="openai_ros_multirobot",
+                               rel_path_from_package_to_file="src/openai_ros/task_envs/multirobot_catch/config",
+                               yaml_file_name="multirobot_catch.yaml")
+
+        # Here we will add any init functions prior to starting the MyRobotEnv
+        super(CatchEnv, self).__init__(ros_ws_abspath)
+
+        # Only variable needed to be set here
+        number_actions = rospy.get_param('/turtlebot2/n_actions')
+        self.action_space = spaces.Discrete(number_actions)
+        
+        # We set the reward range, which is not compulsory but here we do it.
+        self.reward_range = (-numpy.inf, numpy.inf)
+        
+        
+        
+        # Actions and Observations
+        self.linear_forward_speed = rospy.get_param('/turtlebot2/linear_forward_speed')
+        self.linear_turn_speed = rospy.get_param('/turtlebot2/linear_turn_speed')
+        self.angular_speed = rospy.get_param('/turtlebot2/angular_speed')
+        
+        
+        # We only use two integers
+        self.observation_space = spaces.Box(low=0, high=255, shape= (640, 480, 3))
+        
+        rospy.logdebug("ACTION SPACES TYPE===>"+str(self.action_space))
+        rospy.logdebug("OBSERVATION SPACES TYPE===>"+str(self.observation_space))
+        
+        # Rewards
+        self.catch_reward = rospy.get_param("/turtlebot2/catch_reward")
+        self.cooperative_catch_reward = rospy.get_param("/turtlebot2/cooperative_catch_reward")
+        self.time_penelty = rospy.get_param("/turtlebot2/time_penelty")
+        self.max_x = rospy.get_param("/turtlebot2/max_x") 
+        self.max_y = rospy.get_param("/turtlebot2/max_y") 
+        self.min_x = rospy.get_param("/turtlebot2/min_x") 
+        self.min_y = rospy.get_param("/turtlebot2/min_y") 
+
+        self.cumulated_steps = 0.0
+        
+        self.init_linear_forward_speed = 0.0
+        self.init_linear_turn_speed = 0.0
+        self.win = [0,0,0]
+
+
+    def _set_init_pose(self):
+        """Sets the Robot in its init pose
+        """
+        for ii in range(1,4):
+            self.move_base(ii, self.init_linear_forward_speed,
+                            self.init_linear_turn_speed,
+                            sleep_time=0,
+                            epsilon=0.05,
+                            update_rate=10)
+
+        return True
+
+
+    def _init_env_variables(self):
+        """
+        Inits variables needed to be initialised each time we reset at the start
+        of an episode.
+        :return:
+        """
+        # For Info Purposes
+        self.cumulated_reward = 0.0
+        # Set to false Done, because its calculated asyncronously
+        self._episode_done = False
+        
+        camera_data = self.get_camera_rgb_image_raw(1)
+
+
+    def _set_action(self, action):
+        """
+        This set action will Set the linear and angular speed of the turtlebot2
+        based on the action number given.
+        :param action: The action integer that set s what movement to do next.
+        """
+        
+        rospy.logdebug("Start Set Action ==>"+str(action))
+        # We convert the actions to speed movements to send to the parent class CubeSingleDiskEnv
+        for ii in range(len(action)):
+            if action[ii] == 0: #FORWARD
+                linear_speed = self.linear_forward_speed
+                angular_speed = 0.0
+                self.last_action = "FORWARDS"
+            elif action[ii] == 1: #LEFT
+                linear_speed = self.linear_turn_speed
+                angular_speed = self.angular_speed
+                self.last_action = "TURN_LEFT"
+            elif action[ii] == 2: #RIGHT
+                linear_speed = self.linear_turn_speed
+                angular_speed = -1*self.angular_speed
+                self.last_action = "TURN_RIGHT"
+
+            
+            # We tell TurtleBot2 the linear and angular speed to set to execute
+            self.move_base(ii+1,linear_speed, angular_speed, epsilon=0.05, update_rate=10)
+            
+            rospy.logdebug("END Set Action ==>"+str(action))
+
+    def _get_obs(self):
+        """
+        Here we define what sensor data defines our robots observations
+        To know which Variables we have acces to, we need to read the
+        TurtleBot2Env API DOCS
+        :return:
+        """
+        rospy.logdebug("Start Get Observation ==>")
+        # We get the laser scan data
+        observations = [self.get_camera_rgb_image_raw(1),self.get_camera_rgb_image_raw(2),self.get_camera_rgb_image_raw(3)]
+        rospy.logdebug("END Get Observation ==>")
+        return observations
+        
+
+    def _is_done(self, observations):
+        r_robot = 0.351/2
+        r_ball = 0.2
+        p = 0.1
+        self._episode_done = False
+        if self.step_number > 500:
+            self._episode_done = True
+            rospy.logerr("To much steps==>")
+        else:
+            for ii in range (3):
+                current_position = numpy.array(self.get_robot_position(ii+1))
+                prey_position = numpy.array(self.get_prey_position())
+                if (r_robot + r_ball + p) <= numpy.linalg.norm(current_position - prey_position):
+                    self.win[ii] = 1
+                    self._episode_done = True
+            
+
+        return self._episode_done
+
+    def _compute_reward(self, observations, done):
+        reward = [0.0, 0.0, 0.0]
+        
+        prey_position = numpy.array(self.get_prey_position())
+        r_robot = 0.351/2
+        r_ball = 0.2
+        p = 0.1
+
+        if not done:
+            for ii in range(3):
+                current_position = numpy.array(self.get_robot_position(ii+1))
+                if current_position[0] > self.max_x or current_position[0] < self.min_x or current_position[1] > self.max_y or current_position[1] < self.min_y:
+                    reward[ii] += self.robot_out_of_bounds_penalty
+                
+        else:            
+            if 1 in self.win:
+                for ii in range(3):
+                    if self.win[ii] == 1:
+                        reward[ii] = 100.0*self.catch_reward/(self.step_number+1.0)
+                    else:
+                        reward[ii] = 100.0*self.cooperative_catch_reward/(self.step_number+1.0)
+            else:
+                reward = self.time_penelty
+
+
+
+
+        rospy.logdebug("reward=" + str(reward))
+        
+        return reward
+
+
+    # Internal TaskEnv Methods
